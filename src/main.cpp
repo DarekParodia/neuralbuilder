270b003985d9adfebe646e8cd0678ebcb6d01917
DarekParodia <darekparodia@gmail.com>
2026-05-21 10:49:12 +0200

    neurons workin

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,30 @@
 #include "logger.h"
+#include "neuron.h"
 
-int main() { logger->info("Welcome to the Neural Builder"); }
+#include <vector>
+
+int main() {
+    constexpr int                        neuronN = 6;
+    constexpr int                        dataN   = 4;
+    std::vector<float>                   input   = { 2.0f, 1.0f, 0.5f, 0.0f };
+
+    Eigen::Matrix<float, neuronN, dataN> weights1;
+    weights1 << 0.2f, 0.8f, -0.3f, 1.1f,
+    -0.5f, 0.1f, 0.9f, -0.7f,
+    1.3f, -0.4f, 0.6f, 0.2f,
+    -0.1f, 1.5f, -0.8f, 0.3f,
+    0.7f, -0.9f, 0.4f, -1.2f,
+    -0.6f, 0.3f, 1.0f, -0.5f;
+
+    Eigen::Matrix<float, 2, 6> weights2;
+    weights2 << 0.3f, -0.7f, 0.5f, -0.2f, 0.9f,
+    -0.4f, 0.3f, -0.7f, 10.5f, -0.2f, 0.9f, -0.4f;
+
+    neural::layer<neuronN, dataN> lay1(input.data(), &weights1, neural::activations::relu);
+    neural::layer<2, neuronN>     lay2(lay1.outputs, &weights2, neural::activations::relu);
+
+    lay1.process();
+    lay2.process();
+
+    logger->info("Output: {:.4f}, {:.4f}", lay2.outputs[0], lay2.outputs[1]);
+}
--- src/neuron.h
+++ src/neuron.h
@@ -1,17 +1,44 @@
 #pragma once
 
+#include <Eigen/Dense>
+
 namespace neural {
 
-    template <int InputNeuronN>
-    struct neuron {
-            float *inputs;
-            float  output;
+    namespace activations {
+        void relu(float *input, float *output, int n) {
+            for(int i = 0; i < n; i++)
+                output[i] = std::max(0.0f, input[i]);
+        }
+    }; // namespace activations
 
-            float (*activation_func)(float x);
-            float (*summing_func)();
+    template <int NeuronN, int InputN>
+    struct layer {
+            float *inputs;
+            float  sums[NeuronN];
+            float  outputs[NeuronN];
+
+            // n column = wieights of n neuron
+            Eigen::Matrix<float, NeuronN, InputN> *weights;
+
+            void (*activation_func)(float *input, float *output, int n);
+
+            void sum(const float *input, float *output, const Eigen::Matrix<float, NeuronN, InputN> &w) {
+                Eigen::Map<const Eigen::Vector<float, InputN>> in_vec(input);
+                Eigen::Map<Eigen::Vector<float, NeuronN>>      out_vec(output);
+
+                out_vec = w * in_vec; // (NeuronN x InputN) * (InputN) = (NeuronN)
+            }
 
             void process() {
-                this->output = activation_func(summing_func());
+                // Firstly add weights to neurons
+                sum(inputs, sums, *weights);
+                activation_func(sums, outputs, NeuronN);
+            }
+
+            layer(float *inputs, Eigen::Matrix<float, NeuronN, InputN> *weights, void (*activation_func)(float *input, float *output, int n)) {
+                this->inputs          = inputs;
+                this->weights         = weights;
+                this->activation_func = activation_func;
             }
     };
 } // namespace neural
